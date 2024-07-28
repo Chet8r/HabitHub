ca28789057f235719c4d2151ac7a02ed47a9bba9
C Rajesh <cr329@student.le.ac.uk>
2024-07-28 13:02:17 +0100

    Added authgraud routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,35 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Habit from "./components/Habits/Habit";
 import Nav from "./components/Shared/Nav";
-import "./index.css";
-import store from "./components/Habits/Shared/store";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
+import "./index.css";
+import store from "./components/Habits/Shared/store";
+import { AuthProvider } from "./components/User/AuthContext";
+import ProtectedRoute from "./components/User/ProtectedRoute";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <Nav />
-                <Habit />
-              </>
-            }
-          />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoute />}>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Nav />
+                    <Habit />
+                  </>
+                }
+              />
+            </Route>
+          </Routes>
+        </Router>
+      </AuthProvider>
     </Provider>
   );
 }
